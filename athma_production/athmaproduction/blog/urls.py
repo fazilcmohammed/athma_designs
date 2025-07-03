2b2a7fc4551b66b8c1d56547c9b29515767cb9c3
fazilcmohammed <crackthedata@gmail.com>
2025-07-03 23:41:22 +0530

    updated athma website

--- athma_production/athmaproduction/blog/urls.py
+++ athma_production/athmaproduction/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BlogListView, BlogDetailView
+from .views import BlogListView, BlogDetailView, HomeView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('blog/', BlogListView.as_view(), name='blog'),
     path('<slug:slug>/', BlogDetailView.as_view(), name='blog_detail'),
+    path('', HomeView.as_view(), name='home'),
 ]
--- athma_production/athmaproduction/blog/views.py
+++ athma_production/athmaproduction/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from . models import BlogPost
 # Create your views here.
 
@@ -14,3 +14,11 @@ class BlogDetailView(DetailView):
     model = BlogPost
     template_name = 'core/blog_detail.html'
     context_object_name = 'post'
+
+class HomeView(TemplateView):
+    template_name = 'core/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_blogs'] = BlogPost.objects.all().order_by('-created_at')[:2]
+        return context
